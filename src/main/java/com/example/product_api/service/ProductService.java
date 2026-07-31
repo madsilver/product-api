0e0fec9594a6aedd5f99941f89de1bc410ab98d2
Rodrigo Prata <rbpsilver@gmail.com>
2026-07-31 11:20:25 +0100

    create service

---
+++ src/main/java/com/example/product_api/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.product_api.service;
+
+import com.example.product_api.entity.Product;
+import com.example.product_api.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
